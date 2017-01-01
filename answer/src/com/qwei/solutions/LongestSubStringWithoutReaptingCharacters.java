095cba19b08cb5b5bacd076a4c8c490b29eed3bf
Wei, Qi <qwei1@paypal.com>
2016-12-31 21:10:10 -0800

    Added hint for LongestSubStringWithoutReaptingCharacter

--- answer/src/com/qwei/solutions/LongestSubStringWithoutReaptingCharacters.java
+++ answer/src/com/qwei/solutions/LongestSubStringWithoutReaptingCharacters.java
@@ -6,10 +6,9 @@ import java.util.Set;
 /**
  * Given a string, find the length of the longest substring without repeating characters.
  *
- * Examples:
- * Given "abcabcbb", the answer is "abc", which the length is 3.
- * Given "bbbbb", the answer is "b", with the length of 1.
- * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ * Examples: Given "abcabcbb", the answer is "abc", which the length is 3. Given "bbbbb", the answer
+ * is "b", with the length of 1. Given "pwwkew", the answer is "wke", with the length of 3. Note
+ * that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class LongestSubStringWithoutReaptingCharacters {
   public static int lengthOfLongestSubstring(String s) {
@@ -23,11 +22,12 @@ public class LongestSubStringWithoutReaptingCharacters {
     int[] map = new int[256];
 
     while (i < s.length()) {
-      while (j<s.length() && map[s.charAt(j)] == 0) {
+      while (j < s.length() && map[s.charAt(j)] == 0) {
         map[s.charAt(j)] = 1;
-        max = Math.max(max, j-i+1);
+        max = Math.max(max, j - i + 1);
         j++;
       }
+      // Before the next round, reset the current round starting letter's position in map.
       map[s.charAt(i)] = 0;
       i++;
     }
