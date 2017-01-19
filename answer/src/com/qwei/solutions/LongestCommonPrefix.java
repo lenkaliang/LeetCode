add173f6168c641f4eb3b34b3cb917fb7f69952e
Wei, Qi <qwei1@paypal.com>
2017-01-19 00:36:50 -0800

    longest common prefix

---
+++ answer/src/com/qwei/solutions/LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+package com.qwei.solutions;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+public class LongestCommonPrefix {
+  public static String longestCommonPrefix(String[] strs) {
+    String longestCommonPrefix = "";
+    if (strs == null || strs.length == 0) {
+      return "";
+    }
+
+    int index = 0;
+    String firstWord = strs[0];        // use the first word as comparing object
+
+    while (index < firstWord.length()) {
+      for (int i=1; i<strs.length; i++) {
+        String word = strs[i];
+        if (index >= word.length() || firstWord.charAt(index) != word.charAt(index)) {
+          // When hitting a shorter string or the current char of word referenced by index does not equal the char from comparing object
+          // return previously calculated longest common prefix
+          return longestCommonPrefix;
+        }
+      }
+      longestCommonPrefix += firstWord.charAt(index);     // if the current char pass the above check, go ahead and take the new to longest common prefix
+      index++;
+    }
+
+    return longestCommonPrefix;
+  }
+}
