745077dc64bb2e033e346398444f318365d932ed
Wei, Qi <qwei1@paypal.com>
2017-01-20 00:09:12 -0800

    Unfinished phone number to letter combination problem

---
+++ answer/src/com/qwei/solutions/LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,18 @@
+package com.qwei.solutions;
+
+import java.util.List;
+
+/**
+ * Given a digit string, return all possible letter combinations that the number could represent. A
+ * mapping of digit to letters (just like on the telephone buttons) is given below.
+ *
+ * e.g. Input:Digit string "23" Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ *
+ * Although the above answer is in lexicographical order, your answer could be in any order you
+ * want.
+ */
+public class LetterCombinationsOfPhoneNumber {
+  public static List<String> letterCombinations(String digits) {
+
+  }
+}
