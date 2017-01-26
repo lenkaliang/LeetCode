4652feb44a023c0ea2e616b74ff0bc26bf4aa8c1
Qi Wei <qiwei@Qis-MacBook-Pro.local>
2017-01-26 01:01:57 -0800

    4 more problems

---
+++ answer/src/com/qwei/utils/TreeNode.java
@@ -0,0 +1,14 @@
+package com.qwei.utils;
+
+/**
+ * Created by qiwei on 1/26/17.
+ */
+public class TreeNode {
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  public TreeNode(int val) {
+    this.val = val;
+  }
+}
