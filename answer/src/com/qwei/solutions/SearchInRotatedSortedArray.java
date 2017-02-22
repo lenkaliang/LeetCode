c521864b0e943702b6a6ab580af8aed8e50f06ed
Qi Wei <qiwei@Qis-MacBook-Pro.local>
2017-02-21 22:40:38 -0800

    Search In Rotated Sorted Array

--- answer/src/com/qwei/solutions/SearchInRotatedSortedArray.java
+++ answer/src/com/qwei/solutions/SearchInRotatedSortedArray.java
@@ -1,7 +1,51 @@
 package com.qwei.solutions;
 
 /**
- * Created by qiwei on 2/19/17.
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ *
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
  */
 public class SearchInRotatedSortedArray {
+  public static int serach(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }
+
+    int start = 0, end = nums.length-1;
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      if (start == mid) {         // start == mid does not indicates that mid == end
+        start =  mid + 1;
+        continue;
+      }
+
+      if (mid == end) {
+        end = mid - 1;
+        continue;
+      }
+
+      if (nums[start] < nums[mid]) {
+        if (nums[start] <= target && target < nums[mid]) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      } else {
+        if (nums[mid] < target && target <= nums[mid]) {
+          start = mid + 1;
+        } else {
+          end = mid -1;
+        }
+      }
+    }
+
+    return -1;
+  }
 }
