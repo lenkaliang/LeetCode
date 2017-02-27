deb71fd490af58814bc8afd0e8b3ad54dbb673e1
Wei, Qi <qwei1@paypal.com>
2017-02-27 15:29:55 -0800

    ReverseLinkedListII

---
+++ answer/src/com/qwei/solutions/ReverseLinkedListII.java
@@ -0,0 +1,21 @@
+package com.qwei.solutions;
+
+import com.qwei.utils.ListNode;
+
+/**
+ * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+ *
+ * For example:
+ * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ * return 1->4->3->2->5->NULL.
+ *
+ * Note:
+ * Given m, n satisfy the following condition:
+ * 1 ≤ m ≤ n ≤ length of list.
+ */
+public class ReverseLinkedListII {
+  public static ListNode reverse(ListNode head, int m, int n) {
+    // TO DO
+    return null;
+  }
+}
